21f14ec5de847e3ee77dd2afca6424b2f6c43afa
이경민 <cow970814@naver.com>
2022-04-12 18:37:02 +0900

    Initial Commit

---
+++ shareDock/shareDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  shareDockApp.swift
+//  shareDock
+//
+//  Created by 이경민 on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct shareDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
